9d9ac2f9a095afda2d950293f3dbda45d2b0b338
karineWFT <karinehaleksanyan@gmail.com>
2017-07-22 18:17:25 +0400

    controller tests in progress

--- src/test/java/am/aca/orgflix/controller/unit/FilmControllerMockTest.java
+++ src/test/java/am/aca/orgflix/controller/unit/FilmControllerMockTest.java
@@ -3,6 +3,7 @@ package am.aca.orgflix.controller.unit;
 import am.aca.orgflix.BaseUnitTest;
 import am.aca.orgflix.controller.FilmController;
 import am.aca.orgflix.entity.Cast;
+import am.aca.orgflix.entity.Film;
 import am.aca.orgflix.entity.Genre;
 import am.aca.orgflix.service.CastService;
 import am.aca.orgflix.service.FilmService;
@@ -21,6 +22,7 @@ import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -50,6 +52,7 @@ public class FilmControllerMockTest extends BaseUnitTest {
 
     private Cast cast = new Cast("Thandie Newton");
     private List<Cast> casts = new ArrayList<>();
+    private List<Film> films = new ArrayList<>();
 
     @Before
     public void setUp() {
@@ -72,6 +75,8 @@ public class FilmControllerMockTest extends BaseUnitTest {
         mockMvc.perform(get("/addFilm").session(session))
                 .andExpect(view().name("newFilm"))
                 .andExpect(model().attribute("actors", casts));
+
+//        verify(castServiceMock, tm)
     }
 
     /**
@@ -97,14 +102,6 @@ public class FilmControllerMockTest extends BaseUnitTest {
         mockMvc.perform(get("/addFilm").session(session))
                 .andExpect(view().name("error"))
                 .andExpect(model().attributeExists("message"));
-    }
-
-    @Test
-    public void createFilm_Valid_Success() throws Exception {
-        casts.add(cast);
-
-        session.setAttribute("userId", 1);
-        mockMvc.perform(post("/film/addResult?title=").session(session));
     }
 
     /**
@@ -147,15 +144,55 @@ public class FilmControllerMockTest extends BaseUnitTest {
                 .andExpect(model().attributeExists("message"));
     }
 
+    /**
+     * @see FilmController#searchFilm(HttpSession, String, int, int, int, String, int, int)
+     */
     @Test
     public void searchFilmResult_Valid_Success() throws Exception {
         session.setAttribute("userId", 1);
 
-//        when(filmServiceMock.getFilteredFilms())
+        when(filmServiceMock.getFilteredFilms("test", 0, 0, false, "test", 0, 0)).thenReturn(films);
 
         mockMvc.perform(post("/searchFilmResult").param("title", "test")
                 .param("startYear", "0").param("finishYear", "0")
+                .param("hasOscar", "0")
                 .param("director", "test").param("actorId", "0")
-                .param("genre", "0")).andExpect(view().name("index"));
+                .param("genre", "0")
+                .session(session)).andExpect(view().name("index"))
+                .andExpect(model().attribute("films", films))
+                .andExpect(model().attribute("page", "index"));
+    }
+
+    /**
+     * @see FilmController#searchFilm(HttpSession, String, int, int, int, String, int, int)
+     */
+    @Test
+    public void searchFilmResult_InvalidUser_Fail() throws Exception {
+        session.setAttribute("userId", -1);
+
+        mockMvc.perform(post("/searchFilmResult").param("title", "test")
+                .param("startYear", "0").param("finishYear", "0")
+                .param("hasOscar", "0")
+                .param("director", "test").param("actorId", "0")
+                .param("genre", "0")
+                .session(session)).andExpect(view().name("error"))
+                .andExpect(model().attribute("message", "You are not logged in, please first login"));
+    }
+
+    /**
+     * @see FilmController#searchFilm(HttpSession, String, int, int, int, String, int, int)
+     */
+    @Test
+    public void searchFilmResult_ServiceError_Fail() throws Exception {
+        session.setAttribute("userId", 1);
+
+        when(filmServiceMock.getFilteredFilms("test", 0, 0, false, "test", 0, 0)).thenThrow(RuntimeException.class);
+
+        mockMvc.perform(post("/searchFilmResult").param("title", "test")
+                .param("startYear", "0").param("finishYear", "0")
+                .param("hasOscar", "0")
+                .param("director", "test").param("actorId", "0")
+                .param("genre", "0")
+                .session(session)).andExpect(view().name("error"));
     }
 }
